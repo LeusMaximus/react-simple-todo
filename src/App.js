867403dee597c20974e7834d084da0748bb7139e
LeusMaximus <leus.maximus@gmail.com>
2018-04-27 09:39:48 +0300

    Added initial JSX structure for todo

--- src/App.js
+++ src/App.js
@@ -5,6 +5,18 @@ class App extends Component {
     return (
       <div>
         <h1>Simple React TODO</h1>
+
+        <form>
+          <input type="text" />
+          <button type="submit">Add</button>
+        </form>
+
+        <ul>
+          <li>
+            <span>First todo</span>
+            <button type="button">&times;</button>
+          </li>
+        </ul>
       </div>
     );
   }
